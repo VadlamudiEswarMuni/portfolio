c959ad16f7489ce9e156962f2a102f9212aeaf9c
Vadlamudi Eswar Muni <vadlamudieswarmuni@gmail.com>
2026-05-07 21:23:02 +0530

    Fixed Projects section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,24 @@
 const Hero = () => {
   return (
     <section className="text-center py-20 px-6 bg-gradient-to-r from-blue-950 via-purple-900 to-pink-700">
-      <img
-        src="/profile.jpg"
-        alt="profile"
-        className="w-40 h-40 rounded-full mx-auto border-4 border-pink-500 shadow-[0_0_40px_#ff00ff]"
-      />
+      
+      <div className="flex justify-center">
+        <div className="relative">
 
-      <h1 className="text-5xl md:text-7xl font-bold mt-8 bg-gradient-to-r from-cyan-400 via-pink-400 to-orange-400 bg-clip-text text-transparent">
+          <div className="absolute inset-0 rounded-full bg-pink-500 blur-3xl opacity-60 animate-pulse"></div>
+
+          <div className="p-2 rounded-full bg-gradient-to-r from-cyan-400 via-pink-500 to-orange-400 shadow-[0_0_60px_rgba(255,0,255,0.8)]">
+            <img
+              src="/profile.jpg"
+              alt="profile"
+              className="w-64 h-64 md:w-80 md:h-80 rounded-full object-cover border-4 border-white"
+            />
+          </div>
+
+        </div>
+      </div>
+
+      <h1 className="text-5xl md:text-7xl font-bold mt-10 bg-gradient-to-r from-cyan-400 via-pink-400 to-orange-400 bg-clip-text text-transparent">
         Vadlamudi Eswar Muni
       </h1>
 
@@ -19,9 +30,9 @@ const Hero = () => {
         Passionate Frontend and Full Stack Developer skilled in React.js,
         Node.js, Express.js, MongoDB, and modern web technologies.
       </p>
+
     </section>
   );
 };
 
 export default Hero;
-
