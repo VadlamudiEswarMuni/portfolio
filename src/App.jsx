b5ee688e1ffc5ce11b560a98342233e0a1b2297a
Vadlamudi Eswar Muni <vadlamudieswarmuni@gmail.com>
2026-05-07 19:04:56 +0530

    Corrected section order

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import About from "./components/About";
+import Hero from "./components/Hero";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 import Education from "./components/Education";
-import Achievements from "./components/Achievements";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <div className="bg-[#020b5c] text-white min-h-screen">
       <Hero />
       <Skills />
       <Experience />
@@ -16,7 +16,7 @@ function App() {
       <Education />
       <Contact />
       <Footer />
-    </>
+    </div>
   );
 }
 
