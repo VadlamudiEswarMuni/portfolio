b9b10d11958e64af48dfef927b061bb1d39b9bc6
Vadlamudi Eswar Muni <vadlamudieswarmuni@gmail.com>
2026-05-07 19:07:17 +0530

    Fixed App imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Education from "./components/Education";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-[#020b5c] text-white min-h-screen">
-      <Hero />
       <Skills />
       <Experience />
       <Projects />
       <Education />
-      <Contact />
       <Footer />
     </div>
   );
